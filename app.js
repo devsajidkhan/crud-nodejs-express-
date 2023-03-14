80b5b65ecc49284c766d03178610a2593a118397
devsajidkhan <sajidkhan32022@gmail.com>
2023-03-14 16:06:34 +0500

    create get api, setheaders, and decrypted password

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ let app = express();
 app.use(bodyParser.json());
 app.use(router)
 let port = 3000;
+
+
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
--- models/users.js
+++ models/users.js
@@ -3,63 +3,93 @@ const{db} = require('../connection')
 const bcrypt = require('bcrypt')
 
 // Define User model
-const User = db.define('testuser', {
-    firstName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: false,
-      validate: {
-        len: [5, 15],
+const User = db.define('users', {
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: false,
+    validate: {
+      len: [5, 15],
+    }
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: false,
+    validate: {
+      len: [4, 15],
+    }
+  },
+  userName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      len: {
+        rgs: [3, 255],
+        msg: 'Name must be between 3 and 255 characters',
       },
-    },
-    lastName: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: false,
-        validate: {
-            len: [4, 15],
-        },
-    },
-    userName: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-        validate: {
-            len: {
-              args: [3, 255],
-              msg: 'Name must be between 3 and 255 characters',
-            },
-        },
-    },
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-        validate: {
-          isEmail: true,
-        },
-    },
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [6, 255], // length must be between 6 and 255 characters
-          isStrongPassword: function (value) {
-            if (!/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{6,}$/.test(value)) {
-              throw new Error('The password must be at least 6 characters long and contain at least one digit, one lowercase letter, one uppercase letter, and one special character.');
-            }
-            }
+    }
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+    }
+  },
+  CountryCode : {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: false,
+    validate: {
+      len: [2, 10],
+    }
+  },
+  city: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: false,
+    validate: {
+      len: [3, 15],
+    }
+  },
+  language: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: false,
+    validate: {
+      len: [3, 15],
+    }
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      len: [6, 255], // length must be between 6 and 255 characters
+      isStrongPassword: function (value) {
+        if (!/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{6,}$/.test(value)) {
+          throw new Error('The password must be at least 6 characters long and contain at least one digit, one lowercase letter, one uppercase letter, and one special character.');
         }
-    },
-    hooks: {
-        type: DataTypes.STRING,
-        beforeCreate: async (User) => {
-        // Hash the password before storing it
-            const salt = await bcrypt.genSalt(10);
-            User.password = await bcrypt.hash(User.password, salt);
-        },
-    }, 
+      }
+    }
+  },
+  createdBy: {
+    type: DataTypes.STRING,
+    allowNull: true,
+    unique: false,
+    validate: {
+      len: [3, 15],
+    }
+  }
 });
+
+User.beforeCreate(async (user) => {
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+});
+
 User.sync()
   .then(() => {
     console.log('Database synchronized successfully.');
@@ -68,4 +98,4 @@ User.sync()
     console.error('Error synchronizing database:', error);
   });
 
-module.exports = User;
+module.exports = {User, bcrypt};
